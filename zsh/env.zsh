4f9aec08425d0700a9591bfac31f0158c0bfb884
André Santos <adgsantos@protonmail.com>
2020-10-11 22:14:33 +0100

    Disabled conda

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,10 +1,6 @@
 PATH="${HOME}/bin:$PATH"
 
 if isdarwin; then
-	if [[ -n `which conda` ]]; then
-		PATH=`conda info --base`/bin:"$PATH"
-	fi
-
 	if [[ -n `which java` ]]; then
 		export JAVA_HOME=`/usr/libexec/java_home -v 11`
 	fi
